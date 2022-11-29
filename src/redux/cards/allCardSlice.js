75ade88126d852bcce3c544aa7a2dcb9d3ff5da9
Shuhad786 <shuhad.loofer@gmail.com>
2022-11-29 15:35:31 +0200

    Update code to save

--- src/redux/cards/allCardSlice.js
+++ src/redux/cards/allCardSlice.js
@@ -1,48 +1,25 @@
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const DISPLAY_MISSION = 'space-traveler/missions/DISPLAY_MISSION';
-const baseUrl = 'https://api.spacexdata.com/v3/missions';
+const DISPLAY_ALL = 'space-traveler/missions/DISPLAY_ALL';
+const baseUrl = 'https://api.openbrewerydb.org/breweries';
 
-export const displayMission = createAsyncThunk((DISPLAY_MISSION), async () => {
+export const displayCountries = createAsyncThunk((DISPLAY_ALL), async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 });
 
-const missionSlice = createSlice({
-  name: 'mission',
+const CountriesSlice = createSlice({
+  name: 'continent',
   initialState: {
-    missions: [],
-  },
-  reducers: {
-    joinMission: (state, action) => {
-      const update = state;
-      const newState = update.missions.map((mission) => {
-        if (mission.mission_id === action.payload) {
-          return { ...mission, reserved: true };
-        }
-        return mission;
-      });
-      update.missions = newState;
-    },
-    cancelMission: (state, action) => {
-      const update = state;
-      const newState = update.missions.map((mission) => {
-        if (mission.mission_id === action.payload) {
-          return { ...mission, reserved: false };
-        }
-        return mission;
-      });
-      update.missions = newState;
-    },
+    countries: [],
   },
   extraReducers: (builder) => {
-    builder.addCase(displayMission.fulfilled, (state, action) => {
+    builder.addCase(displayCountries.fulfilled, (state, action) => {
       const update = state;
-      update.missions = action.payload.map((mission) => ({ ...mission, reserved: false }));
+      update.countries = action.payload.map((item) => ({ ...item }));
     });
   },
 });
 
-export const { joinMission, cancelMission } = missionSlice.actions;
-export default missionSlice;
+export default CountriesSlice;
